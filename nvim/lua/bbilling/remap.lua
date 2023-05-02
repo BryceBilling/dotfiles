032444bbd75757e932df5f2e820e7de8ed39cd83
Bryce Billing <brycerbilling@gmail.com>
2023-05-02 23:13:58 +0200

    Added dashboard

--- nvim/lua/bbilling/remap.lua
+++ nvim/lua/bbilling/remap.lua
@@ -14,7 +14,7 @@ vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
 
 vim.keymap.set("n", "Q", "<nop>")
 vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
-vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
+vim.keymap.set("n", "<leader>rf", vim.lsp.buf.format)
 
 vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
 vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
@@ -25,6 +25,10 @@ vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
 vim.keymap.set("n", "<leader>vpp", "<cmd>e ~/dotfiles/nvim/lua/bbilling/packer.lua");
+
+vim.keymap.set("n", "<leader>ds", function()
+	vim.cmd("Dashboard")
+end)
 
 vim.keymap.set("n", "<leader><leader>", function()
 	vim.cmd("so")
